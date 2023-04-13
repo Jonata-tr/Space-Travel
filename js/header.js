6a3ec6ac1240ff45cfc6d8618e4a0fce3d45f644
Jonatan <jonatatrindasilva@gmail.com>
2023-04-13 11:23:15 -0300

    Pequenas alterações e correções

--- js/header.js
+++ js/header.js
@@ -1,22 +1,21 @@
-let counter = 0;
+const mobMenu = document.querySelector(".header-menu");
+const hamburguer = document.querySelector(".mob-menu");
 
-function activeMenu() {
-  const mobMenu = document.querySelector(".header-menu");
-  const hamburguer = document.querySelector(".mob-menu");
-  
-  console.log(hamburguer);
-  
-  if (counter == 0) {
-    hamburguer.src = "/assets/shared/icon-close.svg";
-    mobMenu.classList.toggle("show");
-    counter = 1;
-    return;
-  }
-  
-  hamburguer.src = "/assets/shared/icon-hamburger.svg";
+function activeMenu() { 
   mobMenu.classList.toggle("show");
-  counter = 0;
+  if (mobMenu.classList.contains('show')) {
+    hamburguer.src = "/assets/shared/icon-close.svg";
+    return
+  }
+    hamburguer.src = "/assets/shared/icon-hamburger.svg";
 }
 
+document.addEventListener('click', function(event) {
+  var target = event.target;
+  if (!mobMenu.contains(target) && !hamburguer.contains(target)) {
+    mobMenu.classList.remove('show');
+    hamburguer.src = "/assets/shared/icon-hamburger.svg";
+  }
+});
 
 
--- js/tec.js
+++ js/tec.js
@@ -2,29 +2,29 @@ const tecnologias =[
   {
     tipo: 'LAUNCH VEHICLE',
     texto: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",   
-    img: `<picture>
+    img: `
             <source media="(min-width:801px)" srcset="/assets/technology/image-launch-vehicle-portrait.jpg">
             <source media="(max-width:800px)" srcset="/assets/technology/image-launch-vehicle-landscape.jpg">
             <img src="/assets/technology/image-launch-vehicle-portrait.jpg" alt="">
-          </picture>`
+          `
   },
   {
     tipo: 'SPACEPORT',
     texto:'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
-    img:`<picture>
+    img:`
     <source media="(min-width:801px)" srcset="/assets/technology/image-spaceport-portrait.jpg">
     <source media="(max-width:800px)" srcset="/assets/technology/image-spaceport-landscape.jpg">
     <img src="/assets/technology/image-spaceport-portrait.jpg" alt="">
-  </picture>`
+  `
   },
   {
     tipo: 'SPACE CAPSULE',
     texto:"A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-    img:`<picture>
+    img:`
     <source media="(min-width:801px)" srcset="/assets/technology/image-space-capsule-portrait.jpg">
     <source media="(max-width:800px)" srcset="/assets/technology/image-space-capsule-landscape.jpg">
     <img src="/assets/technology/image-space-capsule-portrait.jpg" alt="">
-    </picture>`
+    `
   }
 
 ]
@@ -32,7 +32,7 @@ const tecnologias =[
 function tec() {
   const tipo= document.querySelector('.tec-name')
   const texto = document.querySelector('.tec-text')
-  const img = document.querySelector('.tec-img')
+  const img = document.querySelector('.tec-img picture')
 
   const teste = document.querySelectorAll("input[type='radio']")
   for(let i= 0; i < teste.length; i++){
